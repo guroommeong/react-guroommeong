ad76ba5845fdc4e5bc6612f3d14a07d82cdd6e7d
Kimjaejung <67777124+baebang@users.noreply.github.com>
2024-10-31 15:36:14 +0900

    feat) 홈화면 삭제

--- src/features/home/screen.tsx
+++ src/features/home/screen.tsx
@@ -1,20 +1,9 @@
-import React, { useState } from 'react';
-import { HB2 } from '../../styled/Typography';
-
-import CheckModal from '../../component/modal/checkModal';
+import React from 'react';
 
 const HomeScreen = () => {
-  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
-
-  const toggleModal = () => setModalIsOpen(prev => !prev);
-
   return (
     <div>
-      <CheckModal isOpen={modalIsOpen} toggleModal={toggleModal} modalContent={'??'} />
       <p>홈화면 입니다.</p>
-      <button onClick={toggleModal} style={{ width: '400px', height: '400px' }} type="button">
-        <HB2>랄라랄</HB2>
-      </button>
     </div>
   );
 };
