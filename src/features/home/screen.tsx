4437d5cfda7e915d05be0ea295171edc0f1074bb
Kimjaejung <67777124+baebang@users.noreply.github.com>
2024-10-31 17:10:54 +0900

    feat) svg 파일 첨부

--- src/features/home/screen.tsx
+++ src/features/home/screen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Layout } from '../../components/layout/screen';
+import { ReactComponent as Hamburger } from '../../../src/assets/dogImage/landingDogImg.svg';
 
 const HomeScreen = () => {
   return (
     <Layout title={'메인'}>
       <p>홈화면 입니다.</p>
+      <Hamburger />
     </Layout>
   );
 };
