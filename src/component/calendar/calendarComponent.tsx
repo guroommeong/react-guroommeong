0e8c680537de689e4984fffeefd4f6b51c8c99fc
Minsung <skfkgkdlaos@gmail.com>
2024-10-31 20:40:25 +0900

    Calendar schedule (#9)
    * feat) 캘린더 페이지 UI 구현
    
    * fix) HM2 버튼 폰트사이즈 변경
    
    ---------
    
    Co-authored-by: mskim <mskim@avatye.com>

--- src/component/calendar/calendarComponent.tsx
+++ src/component/calendar/calendarComponent.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import { HB2 } from '../../styled/Typography';
+import React, { useState } from 'react';
+import { DateRange } from 'react-date-range';
+import { ko } from 'date-fns/locale';
+
+import { BM1, HB2, HM2 } from '../../styled/Typography';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 const CalendarComponent = () => {
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+    },
+  ]);
+
+  console.debug('state', state);
+
   return (
-    <div>
-      <HB2>dkdkd</HB2>
+    <div
+      style={{
+        marginTop: 16,
+        padding: 20,
+        backgroundColor: '#ECF1FA',
+        flexGrow: 1,
+      }}>
+      <DateRange
+        locale={ko}
+        editableDateInputs={true}
+        onChange={item => {
+          console.debug('onChange item', item);
+
+          setState([
+            {
+              startDate: item.selection.startDate ?? new Date(),
+              endDate: item.selection.endDate ?? new Date(),
+              key: item.selection.key ?? 'selection',
+            },
+          ]);
+        }}
+        moveRangeOnFirstSelection={false}
+        ranges={state}
+      />
     </div>
   );
 };
