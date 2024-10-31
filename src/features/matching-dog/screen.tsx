2a48886834f9a9af7b3b9dbf91a48688c52c6fe2
Kimjaejung <67777124+baebang@users.noreply.github.com>
2024-10-31 22:50:31 +0900

    feat) 매칭 퍼블리싱 완료

--- src/features/matching-dog/screen.tsx
+++ src/features/matching-dog/screen.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import HeaderComponent from '../../component/header/screen';
-import { AICommentContainer, TagContiner } from './styled';
-import { BM1, CB, CM, HB2 } from '../../styled/Typography';
+import {
+  AICommentContainer,
+  Container,
+  DogNameTagContainer,
+  StyledImage,
+  TagContainer,
+  VerticalScrollView,
+  ContainerWrapper,
+} from './styled';
+import { BB1, BM1, CB, CM, HB2, HM2 } from '../../styled/Typography';
+import { ReactComponent as GirlImage } from '../../../src/assets/dogImage/girlIcon.svg';
+import { ReactComponent as BoyImage } from '../../../src/assets/dogImage/boyIcon.svg';
 
 const MarchingDogList = () => {
+  // 랜덤 색상 배열
+  const colors = ['#FEF1D7', '#9BCAAA', '#95B6F2'];
+
+  // 랜덤 색상 생성 함수
+  const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
   return (
     <div
       style={{
@@ -17,13 +33,41 @@ const MarchingDogList = () => {
         backgroundColor: '#ECF1FA',
       }}>
       <HeaderComponent header={'유기견과 매칭하기'} />
+      <div
+        style={{
+          width: 56,
+          height: 30,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#5380D9',
+          borderRadius: '20px',
+          marginTop: '13px',
+        }}>
+        <HB2 style={{ color: '#FFF' }}># 03</HB2>
+      </div>
+
+      <HM2 style={{ marginTop: 10 }}>보호자분과 성향이 맞는 유기견</HM2>
+
+      {/* 구분선 */}
+      <div
+        style={{
+          width: 37,
+          height: 2,
+          backgroundColor: '#7199E7',
+          marginTop: 16,
+          marginBottom: 16,
+        }}
+      />
+
       <AICommentContainer>
         <HB2 color={'gray'}>AI 분석</HB2>
         <BM1 color={'gray'} style={{ padding: '20px' }}>
           안녕하세요, 제주도 여행을 즐기시는 보호자님! 함께 뛰어놀 수 있는 유기견 친구를 찾고 계시는군요. 크기는
-          중요하지 않고적당히 활동적인 친구를 원하시는군요.
+          중요하지 않고 적당히 활동적인 친구를 원하시는군요.
         </BM1>
       </AICommentContainer>
+
       <div style={{ marginTop: '16px', textAlign: 'left', width: '340px', gap: '4px' }}>
         <CM>Keyword</CM>
         <div
@@ -32,27 +76,67 @@ const MarchingDogList = () => {
             gap: '8px',
             flexDirection: 'row',
             marginTop: '8px',
-            overflowX: 'auto', // 가로 스크롤링 허용
-            whiteSpace: 'nowrap', // 줄 바꿈을 방지하여 한 줄로 표시
+            overflowX: 'auto',
+            whiteSpace: 'nowrap',
             scrollbarWidth: 'none',
+            width: '340px',
           }}>
-          <TagContiner>
+          <TagContainer>
             <CB style={{ color: '#FEF1D7' }}>활발함</CB>
-          </TagContiner>
-          <TagContiner>
+          </TagContainer>
+          <TagContainer>
             <CB style={{ color: '#95B6F2' }}>크기무관</CB>
-          </TagContiner>
-          <TagContiner>
+          </TagContainer>
+          <TagContainer>
             <CB style={{ color: '#9BCAAA' }}>적당히 활동적</CB>
-          </TagContiner>
-          <TagContiner>
+          </TagContainer>
+          <TagContainer>
             <CB style={{ color: '#9BCAAA' }}>적당히 활동적</CB>
-          </TagContiner>
-          <TagContiner>
-            <CB style={{ color: '#9BCAAA' }}>적당히 활동적</CB>
-          </TagContiner>
+          </TagContainer>
         </div>
       </div>
+
+      <VerticalScrollView>
+        <ContainerWrapper>
+          {[...Array(6)].map((_, index) => {
+            return (
+              <Container num={index} key={index}>
+                <StyledImage
+                  src="https://media.istockphoto.com/id/1853686056/ko/%EC%82%AC%EC%A7%84/%EC%A7%91%EC%97%90%EC%84%9C-%ED%9C%B4%EC%8B%9D%EC%9D%84-%EC%B7%A8%ED%95%98%EB%8A%94-%EA%B3%A8%EB%93%A0-%EB%A6%AC%ED%8A%B8%EB%A6%AC%EB%B2%84.webp?b=1&s=612x612&w=0&k=20&c=Lt8MCrP1H17sH79PdD0-mLiYswNTMZor53Ea1Clf1CU="
+                  alt="휴식을 취하는 골든 리트리버"
+                />
+                <DogNameTagContainer num={index}>
+                  <BB1>아리</BB1>
+                </DogNameTagContainer>
+                <div
+                  style={{
+                    paddingLeft: '9px',
+                    paddingRight: '9px',
+                    display: 'flex', // Flexbox 사용
+                    flexDirection: 'row', // 수평 정렬
+                    alignItems: 'center', // 세로 가운데 정렬
+                    justifyContent: 'space-between',
+                  }}>
+                  <GirlImage />
+                  <div
+                    style={{
+                      display: 'flex', // Flexbox 사용
+                      flexDirection: 'row', // 수평 정렬
+                      alignItems: 'center', // 세로 가운데 정렬
+                      gap: '8px', // 요소 간의 간격 (필요시 조정)
+                    }}>
+                    <CM>매칭점수</CM>
+                    <BB1>4.5/5</BB1>
+                  </div>
+                </div>
+                <div style={{ width: '100%', textAlign: 'center', justifyContent: 'center', marginTop: '8px' }}>
+                  <BM1>활동이 많은 친구</BM1>
+                </div>
+              </Container>
+            );
+          })}
+        </ContainerWrapper>
+      </VerticalScrollView>
     </div>
   );
 };
--- src/features/matching-dog/styled.ts
+++ src/features/matching-dog/styled.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+const colors = ['#FEF1D7', '#9BCAAA', '#95B6F2'];
+
+const getRandomColor = (num: number) => {
+  return colors[num % colors.length]; // num을 colors 길이로 나눈 나머지를 인덱스로 사용
+};
+
 const AICommentContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -17,10 +23,67 @@ const AICommentContainer = styled.div`
   border-radius: 12px;
 `;
 
-const TagContiner = styled.div`
+const TagContainer = styled.div`
   background-color: #35383f;
   padding: 5px 20px;
   border-radius: 20px;
 `;
 
-export { AICommentContainer, TagContiner };
+const VerticalScrollView = styled.div`
+  width: 400px;
+  height: 450px; /* 스크롤 가능한 높이 설정 */
+  overflow-y: auto; /* 세로 스크롤 활성화 */
+
+  padding: 16px;
+
+  border-radius: 8px;
+  margin-top: 8px;
+  gap: 20px;
+  flex-direction: row;
+`;
+
+const ContainerWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap; /* 아이템을 줄 바꿈하여 2개씩 배치 */
+  gap: 20px;
+  justify-content: center;
+`;
+interface ContainerProps {
+  num: number;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 164px;
+  height: 204px;
+  background-color: ${({ num }) => getRandomColor(num)};
+  border-radius: 8px;
+  position: relative;
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: 50%;
+  object-fit: cover;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+`;
+
+const DogNameTagContainer = styled.div<ContainerProps>`
+  position: absolute;
+  bottom: 101px; /* 사진의 하단에서 약간 위로 배치 */
+  left: 13.5%;
+  transform: translateX(-50%);
+  background-color: ${({ num }) => getRandomColor(num)};
+  border-radius: 4px;
+  padding: 4px 10px;
+`;
+
+export {
+  AICommentContainer,
+  TagContainer,
+  VerticalScrollView,
+  Container,
+  StyledImage,
+  DogNameTagContainer,
+  ContainerWrapper,
+};
